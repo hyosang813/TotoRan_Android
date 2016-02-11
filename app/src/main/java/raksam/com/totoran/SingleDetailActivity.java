a8f8e90b7d09ab3abe8b54e844576e4f62441bdf
hyosang813 <hyosang813@gmail.com>
2016-02-11 23:55:44 +0900

    条件指定画面(マルチ／シングル)の大まかデザイン完了

---
+++ app/src/main/java/raksam/com/totoran/SingleDetailActivity.java
@@ -0,0 +1,13 @@
+package raksam.com.totoran;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SingleDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_detail);
+    }
+}
