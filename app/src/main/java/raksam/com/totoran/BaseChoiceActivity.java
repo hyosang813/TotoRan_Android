1e07da8d0ed49c30c7a1e77146414ec7e65fcd02
hyosang813 <hyosang813@gmail.com>
2016-02-14 17:18:59 +0900

    シングルは枠内ボタン排他制御を実装

--- app/src/main/java/raksam/com/totoran/BaseChoiceActivity.java
+++ app/src/main/java/raksam/com/totoran/BaseChoiceActivity.java
@@ -5,7 +5,6 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ToggleButton;
 
 import java.util.List;
@@ -66,6 +65,11 @@ public class BaseChoiceActivity extends Activity {
 
     //ボタン押下時の挙動
     public void buttonClicked(View v) {
+        //既存のエフェクト音を決して音をならす
+        playSounds(buttonSoundId);
+
+        //別々の処理はこのメソッドを子クラスでOverrideして音はsuperすべき
+
         //idのプレフィックスをキーにsingleなのかmultiなのか判断
         //判断の内容は以下
         // 色
@@ -73,26 +77,45 @@ public class BaseChoiceActivity extends Activity {
         // 枠内でマルチ選択可能かどうか？
         // ？
 
-
-        //既存のエフェクト音を決して音をならす
-        playSounds(v, buttonSoundId);
     }
 
     //クリアボタンの挙動
     public void allClear(View v) {
         //既存のエフェクト音を決して音をならす
-        playSounds(v, clearSoundId);
+        playSounds(clearSoundId);
 
-        //全選択状態をクリア(対応配列も初期化)
+        //全選択状態をクリア
+        //対象がシングルかマルチかを分別
+        String idPrefix = v.getId() == R.id.clear_button_single ? "single" : "multi";
+
+        for (int i = 0; i < 13; i++) {
+            for (int j = 0; j < 3; j++) {
+                //id文字列作成
+                String idStr = idPrefix + "_button" + String.valueOf(i + 1) + "_" + String.valueOf(j + 1);
+                int btnId = getResources().getIdentifier(idStr, "id", getPackageName());
+
+                //ボタンを取得
+                ToggleButton btn = (ToggleButton) findViewById(btnId);
+
+                //ボタンのチェック状態をクリア
+                btn.setChecked(false);
+            }
+        }
     }
 
     //ボタンの音をならす
-    private void playSounds(View v, int part) {
+    private void playSounds(int part) {
         buttonSoundPool.play(part, 1.0F, 1.0F, 0, 0, 1.0F);
     }
 
     //メイン画面に戻る
     public void goBack(View v) {
+        /**
+         * 選択状態がクリアになるアラートメッセージ表示
+         * ボタンの選択状態は勝手にクリアになるけど、対応ArrayはここでAll Falseにする必要あり
+         */
+
+        //戻る
         finish();
     }
 }
--- app/src/main/java/raksam/com/totoran/SingleChoiceActivity.java
+++ app/src/main/java/raksam/com/totoran/SingleChoiceActivity.java
@@ -2,8 +2,9 @@ package raksam.com.totoran;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ToggleButton;
 
 
 public class SingleChoiceActivity extends BaseChoiceActivity {
@@ -16,6 +17,33 @@ public class SingleChoiceActivity extends BaseChoiceActivity {
         //ボタン作成メソッドを呼び出し
         buttonMake("single");
 
+    }
+
+    @Override
+    public void buttonClicked(View v) {
+        super.buttonClicked(v);
+
+        //ボタン押下時がcheckedの場合のみシングルは枠内で１つしか選択できないので排他制御が必要
+        if (!((ToggleButton)v).isChecked()) return;
+
+        //自身のIDを取得
+        int vId = v.getId();
+
+        //トグルボタンのIDの文字列を取得(最後の一文字だけ取っ払う)
+        String btnIdStr = getApplicationContext().getResources().getResourceEntryName(vId).substring(0, 15);
+
+        //自分以外の他２つを非選択状態にする
+        for (int i = 1; i < 4; i++) {
+            //id文字列作成
+            int btnId = getResources().getIdentifier(btnIdStr + String.valueOf(i), "id", getPackageName());
+
+            //自分はスルー
+            if (btnId == vId) continue;
+
+            //チェック状態をクリア
+            ToggleButton btn = (ToggleButton) findViewById(btnId);
+            btn.setChecked(false);
+        }
     }
 
     //「次へ」ボタン押下時はシングル条件指定画面に画面遷移
